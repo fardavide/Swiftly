c7ad27e684282dc1031c4217125a70da9aba89c2
Davide Giuseppe Farella <fardavide@gmail.com>
2023-10-30 19:36:42 +0100

    fix: api

--- CurrencyApi/Sources/CurrencyApi/Endpoint.swift
+++ CurrencyApi/Sources/CurrencyApi/Endpoint.swift
@@ -14,24 +14,24 @@ struct Endpoint {
 extension Endpoint {
   
   static func currencties() -> Endpoint {
-    Endpoint(path: "/currencies")
+    Endpoint(path: "currencies")
   }
   
   static func latestRates() -> Endpoint {
-    Endpoint(path: "/latest")
+    Endpoint(path: "latest")
   }
   
   var url: URL {
     var components = URLComponents()
     components.scheme = "https"
-    components.host = "api.currencyapi.com/v3/"
-    components.path = path
+    components.host = "api.currencyapi.com"
+    components.path = "/v3/\(path)"
     components.queryItems = [URLQueryItem(name: "apikey", value: apiKey)]
     
     return if let url = components.url {
       url
     } else {
-      preconditionFailure("Invalid urs \(components)")
+      preconditionFailure("Invalid url \(components)")
     }
   }
 }
