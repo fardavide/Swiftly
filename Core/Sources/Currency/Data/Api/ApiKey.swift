6584fd92bccbbad9437c3fcac03030a2499a246f
Davide Giuseppe Farella <37957092+fardavide@users.noreply.github.com>
2023-12-10 15:16:14 +0100

    chore: update ApiKey.swift

--- Core/Sources/Currency/Data/Api/ApiKey.swift
+++ Core/Sources/Currency/Data/Api/ApiKey.swift
@@ -1,3 +1,11 @@
-var apiKey: String {
-  fatalError("Insert you API key here")
+class ApiKey {
+  static var currencyApiDotCom: String {
+    fatalError("Insert you API key here")
+  }
+  static var currencyBeaconCom: String {
+    fatalError("Insert you API key here")
+  }
+  static var exchangeRatesDotIo: String {
+    fatalError("Insert you API key here")
+  }
 }
