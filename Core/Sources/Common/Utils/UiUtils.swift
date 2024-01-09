8055272fbe17a231ae6776dce04f7ab502c8af0f
Davide Giuseppe Farella <fardavide@gmail.com>
2024-01-09 11:42:02 +0100

    fix: ignore closeKeyboard on Mac

--- Core/Sources/Common/Utils/UiUtils.swift
+++ Core/Sources/Common/Utils/UiUtils.swift
@@ -1,7 +1,11 @@
+#if canImport(UIKit)
 import UIKit
+#endif
 
 public extension UIApplication {
   func closeKeyboard() {
+#if canImport(UIKit)
     sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
+#endif
   }
 }
