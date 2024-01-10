15ba03715cacb09c029a0e48ef8f03569f520a31
Davide Giuseppe Farella <fardavide@gmail.com>
2024-01-10 18:01:56 +0100

    chore: add pre-build script

---
+++ ci_scripts/ci_pre_xcodebuild.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+source ~/.bash_profile
+echo "Adding API keys"
+
+# Navigate to the directory containing ApiKey.swift
+cd Core/Sources/Currency/Data/Api
+
+# Rewrite ApiKey.swift with the new content
+cat > ApiKey.swift <<EOF
+class ApiKey {
+  static let currencyApiCom = "$CurrencyApiComKey"
+  static let currencyBeaconCom = "$CurrencyBeaconComKey"
+  static let exchangeRatesIo = "$ExchangeRatesIoKey"
+}
+EOF
+
+# Display the updated file for verification (optional)
+cat ApiKey.swift
+
+exit 0
